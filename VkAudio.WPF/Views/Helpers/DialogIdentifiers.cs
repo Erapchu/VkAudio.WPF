6d8a8ef7cc22f93440148bd1181b6f19e9adf65e
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-08 21:10:16 +0700

    Added dialog host

---
+++ VkAudio.WPF/Views/Helpers/DialogIdentifiers.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace VkAudio.WPF.Views.Helpers
+{
+    internal static class DialogIdentifiers
+    {
+        public static string MainWindowName { get; } = "MainWindowDialogHost";
+
+        public static bool IsInDesignMode => DesignerProperties.GetIsInDesignMode(new DependencyObject());
+    }
+}
