cacc4bae9eb8278caa3aa5f8656b4377bdafba40
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-14 20:37:18 +0700

    Moves and fixes

---
+++ VkAudio.WPF/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace VkAudio.WPF.Converters
+{
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+}
