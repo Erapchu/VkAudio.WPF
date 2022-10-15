dc002695917fb63021c9c4736c5ff17d303f5707
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-10-15 14:37:12 +0700

    Added material message box

---
+++ VkAudio.WPF/Views/MaterialMessageBoxContent.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VkAudio.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MaterialMessageBoxContent.xaml
+    /// </summary>
+    internal partial class MaterialMessageBoxContent : UserControl
+    {
+        public MaterialMessageBoxContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
